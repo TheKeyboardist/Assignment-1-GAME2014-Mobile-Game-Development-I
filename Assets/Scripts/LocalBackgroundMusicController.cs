09a005984efefd9f403c160ce1e0bd449cf3de97
Ivan Kravchenko <41616785+TheKeyboardist@users.noreply.github.com>
2020-10-24 22:37:35 -0400

    Parts: 1A, 1B, 1C of the Assignment 1 Part 2

---
+++ Assets/Scripts/LocalBackgroundMusicController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalBackgroundMusicController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.FindGameObjectWithTag("Music").GetComponent<BackgroundMusicController>().StopMusic();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
