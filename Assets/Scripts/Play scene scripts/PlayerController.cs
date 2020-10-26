b09515785bfb9d1e5cd28e206678b7bcdbfa7d55
Ivan Kravchenko <41616785+TheKeyboardist@users.noreply.github.com>
2020-10-26 19:53:38 -0400

    player controller
    1)player movement with joystick input
    2)player animations

---
+++ Assets/Scripts/Play scene scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController controller;
+    public Rigidbody2D rb;
+    public Joystick joystick;
+    public float runSpeed = 2f;
+    Vector2 movemenet;
+    public Animator animator;
+  
+
+    // Update is called once per frame
+    void Update()//INPUT 
+    {
+        transform.localScale = new Vector3(1.0f, 1, 1);
+        if (joystick.Horizontal <= 0)
+        {
+            transform.localScale = new Vector3(-1.0f, 1, 1);
+        }
+        movemenet.x = joystick.Horizontal  * runSpeed;
+        movemenet.y = joystick.Vertical * runSpeed;
+        animator.SetFloat("Horizontal", movemenet.x);
+        animator.SetFloat("Vertical", movemenet.y);
+        animator.SetFloat("Speed", movemenet.sqrMagnitude);
+
+    }
+
+    void FixedUpdate()//OUTPUT
+    {
+        rb.MovePosition(rb.position + movemenet * runSpeed * Time.fixedDeltaTime);
+    }
+}
